ed1f97701d1270fa11c9fd899155914143f3e86c
noba15 <raaschsistemas@gmail.com>
2024-11-22 12:27:23 -0300

    first commit

---
+++ src/app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# app config
+st.set_page_config(page_title="Streamlit Chatbot", page_icon="🤖")
+st.title("Chatbot")
+
+def get_response(user_query, chat_history):
+
+    template = """
+    You are a helpful assistant. Answer the following questions considering the history of the conversation:
+    respond in Portuguese
+    Chat history: {chat_history}
+
+    User question: {user_question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    llm = ChatOpenAI()
+        
+    chain = prompt | llm | StrOutputParser()
+    
+    return chain.invoke({
+        "chat_history": chat_history,
+        "user_question": user_query,
+    })
+
+# session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Ola o que deseja saber?"),
+    ]
+
+    
+# conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+# user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    with st.chat_message("AI"):
+        response = get_response(user_query, st.session_state.chat_history)
+        st.write(response)
+
+    st.session_state.chat_history.append(AIMessage(content=response))
